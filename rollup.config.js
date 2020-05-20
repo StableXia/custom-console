bf12fc9634826eabfc3cb264bb50e6528a14df4a
wxia <wxia@dfocuspace.com>
2020-05-20 17:32:14 +0800

    fix: 777

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const babel = require('rollup-plugin-babel')
 const nodeResolve = require('@rollup/plugin-node-resolve')
+const uglify = require('rollup-plugin-uglify').uglify
+const merge = require('lodash.merge')
 const pkg = require('./package.json')
 
 const extensions = ['.js', '.ts']
@@ -9,21 +11,48 @@ const resolve = function (...args) {
   return path.resolve(__dirname, ...args)
 }
 
-module.exports = {
-  input: resolve('./src/index.ts'),
-  output: {
-    file: resolve('./', pkg.main),
-    format: 'umd',
-    name: 'FECatch',
+// 打包任务的个性化配置
+const jobs = {
+  esm: {
+    output: {
+      format: 'esm',
+      file: resolve(pkg.module),
+    },
   },
-  plugins: [
-    nodeResolve({
-      extensions,
-      modulesOnly: true,
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      extensions,
-    }),
-  ],
+  umd: {
+    output: {
+      format: 'umd',
+      file: resolve(pkg.main),
+      name: 'FECatch',
+    },
+  },
+  min: {
+    output: {
+      format: 'umd',
+      file: resolve(pkg.main.replace(/(.\w+)$/, '.min$1')),
+      name: 'FECatch',
+    },
+    plugins: [uglify()],
+  },
 }
+
+// 从环境变量获取打包特征
+const mergeConfig = jobs[process.env.FORMAT || 'esm']
+
+module.exports = merge(
+  {
+    input: resolve('./src/index.ts'),
+    output: {},
+    plugins: [
+      nodeResolve({
+        extensions,
+        modulesOnly: true,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        extensions,
+      }),
+    ],
+  },
+  mergeConfig
+)
