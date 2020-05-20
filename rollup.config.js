9752a45b38b9697c62becc59bbe253c7f7382a5f
wxia <wxia@dfocuspace.com>
2020-05-20 16:13:11 +0800

    fix: bugs

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const babel = require('rollup-plugin-babel')
+const nodeResolve = require('@rollup/plugin-node-resolve')
+const pkg = require('./package.json')
+
+const extensions = ['.js', '.ts']
+
+const resolve = function (...args) {
+  return path.resolve(__dirname, ...args)
+}
+
+module.exports = {
+  input: resolve('./src/index.ts'),
+  output: {
+    file: resolve('./', pkg.main),
+    format: 'umd'
+  },
+  plugins: [
+    nodeResolve({
+      extensions,
+      modulesOnly: true
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions
+    })
+  ]
+}
