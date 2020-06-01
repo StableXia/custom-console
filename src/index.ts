4e42cf7b210b7709a0923f528ceae2aa76f0cb59
wxia <wxia@dfocuspace.com>
2020-06-01 18:20:33 +0800

    fix: bugs

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * 1、手动开启错误捕捉
+ * 2、根据环境开启配置
+ * 3、错误捕捉上报率
+ * 4、中间件机制
+ */
+
+// export function start() {}
+
+class FECatch {
+  public use(): void {
+    return
+  }
+
+  public start(): void {
+    return
+  }
+}
+
+export default FECatch
