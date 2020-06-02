2a982478e4159bff4ae765cb68240e0dc53d1263
xiawen <xiawen@xiawendeMacBook-Pro.local>
2020-06-02 18:16:32 +0800

    fix: bugs

--- src/helpers/custom-console.ts
+++ src/helpers/custom-console.ts
@@ -2,17 +2,26 @@ const log: Function = console.log;
 const error: Function = console.error;
 const warn: Function = console.warn;
 
-export function customConsoleMixin(ConsoleLog: Function): void {
+export function customConsoleMixin(ConsoleLog: any): void {
   ConsoleLog.prototype.log = (value: any): void => {
-    log(value);
+    if (!ConsoleLog.config.silent) {
+      return;
+    }
+    log(ConsoleLog.config.splitter, value);
   };
 
   ConsoleLog.prototype.error = (value: any): void => {
-    error(value);
+    if (!ConsoleLog.config.silent) {
+      return;
+    }
+    error(ConsoleLog.config.splitter, value);
   };
 
   ConsoleLog.prototype.warn = (value: any): void => {
-    warn(value);
+    if (!ConsoleLog.config.silent) {
+      return;
+    }
+    warn(ConsoleLog.config.splitter, value);
   };
 }
 
